9642dcf8f1b41df93dd4054303ffbfb4bf83ac67
DESKTOP-TEAPK4H\bbang7 <zxas1245@naver.com>
2018-07-18 18:21:56 +0900

    HW#2
    algorithm study

---
+++ hw2-2.py
@@ -0,0 +1,78 @@
+n = int(input('입력할 배열의 길이 : '))
+N = [0 for i in range(n)]
+for i in range(n):
+    num = int(input())
+    N[i] = num
+N.sort()
+K = int(input('K 를 입력하시오 : '))
+
+begin = 0
+end = len(N)-1
+
+def floor(list,begin,end, K):
+    N = list
+    K = K
+    begin = begin
+    end = end
+    middle = (begin+end)//2
+    print(N,K,begin,end,middle)
+    if N[middle] == K:
+        return print(N[middle-1])
+    else:
+        if N[middle] > K:
+            if begin == middle:
+                if N[middle] > K:
+                    return print('-1')
+                else:
+                    return print(N[middle-1])
+            if middle == 0:
+                return print(N[middle])
+            else:
+                return print('1'),floor(list,begin,middle-1,K)
+        elif N[middle] < K:
+            if middle == end:
+                return print(N[middle])
+            else:
+                return print('2'),floor(list, middle + 1, end, K)
+
+
+def ceiling(list,begin,end, K):
+    N = list
+    K = K
+    begin = begin
+    end = end
+    middle = (begin+end)//2
+    if N[middle] == K:
+        return print(N[middle+1])
+    else:
+        if N[middle] > K:
+            if begin == middle:
+                if N[middle] < K:
+                    return print('-1')
+                else:
+                    return print(N[middle])
+            if middle == 0:
+                return print(N[middle])
+            else:
+                return ceiling(list, begin, middle-1, K)
+
+        elif N[middle] < K:
+            if end == middle:
+                try:
+                    if N[middle] < K:
+                        return print(N[middle+1])
+                except:
+                    return print('-1')
+                else:
+                    return print('-1')
+
+            else:
+                return ceiling(list,middle+1,end,K)
+ceiling(N,begin,end,K)
+
+floor(N,begin,end,K)
+
+
+
+
+
---
+++ hw2.py
@@ -0,0 +1,28 @@
+n = int(input('입력할 배열의 길이 : '))
+N = [0 for i in range(n)]
+for i in range(n):
+    num = int(input())
+    N[i] = num
+N.sort()
+K = int(input('K 를 입력하시오 : '))
+
+begin = 0
+end = len(N)
+count = 0
+
+def sol(list,begin,end, K ,count):
+    N = list
+    K = K
+    begin = begin
+    end = end
+    count = count
+    for i in range(begin,end):
+        if begin == end-1:
+            return print('순서쌍 개수 : ',count)
+        if begin == i:
+            continue
+        elif N[begin]+N[i] == K:
+            return sol(N, begin + 1, end, K, count + 1)
+        elif end-1 == i:
+            return sol(N, begin + 1, end, K, count)
+sol(N,begin,end,K,count)
